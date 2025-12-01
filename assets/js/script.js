3f7151afe5e4357c667a15355f3a1a75c34eb0ae
Ali Zaib <az7511730@gmail.com>
2025-12-01 17:43:16 +0500

    Create reusable image-with-text and cards-grid components
    Added reusable classes for image-with-text layouts
    
    Implemented a flexible cards-grid system for consistent section design

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,24 @@ var swiper = new Swiper(".swiper", {
     prevEl: ".swiper-btn-prev",
   },
 });
+
+// var swiper = new Swiper(".swiper", {
+//   slidesPerView: 3,
+//   direction: getDirection(),
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   on: {
+//     resize: function () {
+//       swiper.changeDirection(getDirection());
+//     },
+//   },
+// });
+
+// function getDirection() {
+//   var windowWidth = window.innerWidth;
+//   var direction = window.innerWidth <= 760 ? "vertical" : "horizontal";
+
+//   return direction;
+// }
