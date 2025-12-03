85902aadfe32862f5948bdd0f43c38b883c73d08
Ali Zaib <az7511730@gmail.com>
2025-12-03 12:34:44 +0500

    Add page header and contact form on Contact page
    Added page header for the Contact page
    
    Implemented contact form with input fields and submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,3 +52,13 @@ var teamSwiper = new Swiper(".team-swiper", {
     },
   },
 });
+
+// ===== Google Map =====
+function myMap() {
+  var mapProp = {
+    center: new google.maps.LatLng(51.508742, -0.12085),
+    zoom: 5,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+s;
