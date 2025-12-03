966acfddf74209c9091d3b9fca6c96621fab7e09
Ali Zaib <az7511730@gmail.com>
2025-12-03 13:13:05 +0500

    Add Google Map location, Add subject field to contact form
    Embedded Google Map on the page to show business location
    Added Subject label and input field in the contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,13 +52,3 @@ var teamSwiper = new Swiper(".team-swiper", {
     },
   },
 });
-
-// ===== Google Map =====
-function myMap() {
-  var mapProp = {
-    center: new google.maps.LatLng(51.508742, -0.12085),
-    zoom: 5,
-  };
-  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
-s;
