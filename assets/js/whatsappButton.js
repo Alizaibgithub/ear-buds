883d7d27623d7a23c2fcf664bc187fe7054c6deb
Ali Zaib <az7511730@gmail.com>
2026-01-14 16:48:40 +0500

    Adjusted whatsapp button html

--- assets/js/whatsappButton.js
+++ assets/js/whatsappButton.js
@@ -1,10 +1,8 @@
 document.body.insertAdjacentHTML(
   "beforeend",
   `<!-- ===== Whatsapp Button ===== -->
-<a href="https://wa.me/923019153840" target="_blank" rel="noopener">
-  <button class="btn-circle btn-whatsapp">
+<a href="https://wa.me/923019153840" target="_blank" class="btn-circle btn-whatsapp" rel="noopener">
     <i class="fa-brands fa-whatsapp"></i>
-  </button>
 </a>
 `
 );
