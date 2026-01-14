0295bbef20ed64ae41a479b54ba5e4c5bf239759
Ali Zaib <az7511730@gmail.com>
2026-01-14 14:55:01 +0500

    removed   <!-- Cart Script -->
    Additional comment

---
+++ assets/js/whatsappButton.js
@@ -0,0 +1,7 @@
+document.body.insertAdjacentHTML(
+  "beforeend",
+  ` <!-- ===== Whatsapp Button ===== -->
+<button class="btn-circle btn-whatsapp">
+    <i class="fa-brands fa-whatsapp"></i>
+</button>`
+);
