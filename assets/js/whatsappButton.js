31c0df814e98bbb3525c229ba6ecfd2eb07a5621
Ali Zaib <az7511730@gmail.com>
2026-01-14 15:20:23 +0500

    Added whatsappButton using JS
    Also added links in html files

--- assets/js/whatsappButton.js
+++ assets/js/whatsappButton.js
@@ -1,7 +1,10 @@
 document.body.insertAdjacentHTML(
   "beforeend",
-  ` <!-- ===== Whatsapp Button ===== -->
-<button class="btn-circle btn-whatsapp">
+  `<!-- ===== Whatsapp Button ===== -->
+<a href="https://wa.me/923019153840" target="_blank" rel="noopener">
+  <button class="btn-circle btn-whatsapp">
     <i class="fa-brands fa-whatsapp"></i>
-</button>`
+  </button>
+</a>
+`
 );
