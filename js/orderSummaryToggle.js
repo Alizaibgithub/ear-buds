3210a486baa1af47cb2c6d15a68f02c4d3894d2c
Ali Zaib <az7511730@gmail.com>
2026-02-12 14:46:43 +0500

    Toggle order summary on page resize

--- js/orderSummaryToggle.js
+++ js/orderSummaryToggle.js
@@ -1,9 +1,15 @@
-const orderSummaries = document.querySelectorAll(".order-summary-container");
+const orderSummaries = document.querySelectorAll(".order-summary-details");
 
-orderSummaries.forEach(orderSummary => {
-    if (window.innerWidth > 768) {
-        orderSummary.setAttribute("open", "");
-    } else {
-        orderSummary.removeAttribute("open");
-    }
-});
+const handleResponsiveDetails = () => {
+    orderSummaries.forEach(orderSummary => {
+        if (window.innerWidth > 768) {
+            orderSummary.setAttribute("open", "");
+        } else {
+            orderSummary.removeAttribute("open");
+        }
+    });
+};
+
+handleResponsiveDetails();
+
+window.addEventListener("resize", handleResponsiveDetails);
