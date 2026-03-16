edc66304391018e1c65f6945e0edf4c0caf27846
Ali Zaib <az7511730@gmail.com>
2026-03-16 12:45:39 +0500

    Adjusted attributes all over the site in html code

--- js/scrollToTopButton.js
+++ js/scrollToTopButton.js
@@ -1,7 +1,7 @@
 // ===== Scroll To Top Button =====
 document.body.insertAdjacentHTML(
   "beforeend",
-  `<button class="btn-circle" id="btn-scrollToTop" aria-label="Scroll To Top"><i class="fa-solid fa-angle-up" aria-hidden="true"></i></button>`
+  `<button id="btn-scrollToTop" class="btn-circle" aria-label="Scroll To Top"><i class="fa-solid fa-angle-up" aria-hidden="true"></i></button>`
 );
 
 const scrollBtn = document.getElementById("btn-scrollToTop");
--- js/whatsappButton.js
+++ js/whatsappButton.js
@@ -2,7 +2,7 @@
 document.body.insertAdjacentHTML(
   "beforeend",
   `<!-- ===== Whatsapp Button ===== -->
-<a href="https://wa.me/923019153840" target="_blank" rel="noopener noreferrer" class="btn-circle btn-whatsapp" rel="noopener" aria-label="Contact on WhatsApp">
+<a href="https://wa.me/923019153840" target="_blank" rel="noopener noreferrer" class="btn-circle btn-whatsapp" aria-label="Contact on WhatsApp">
     <i class="fa-brands fa-whatsapp" aria-hidden="true"></i>
 </a>
 `,
